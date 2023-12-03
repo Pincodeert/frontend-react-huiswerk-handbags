5edea2da8f1b35039e8418d625a3edcae4102d4b
Pincodeert <leonie.pin@gmail.com>
2023-12-03 16:03:14 +0100

    buttons gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import './App.css'
 
 function App() {
+
+  function logToTheCollection() {
+      console.log("to the collection");
+  }
+
+  function logShopAllBags() {
+      console.log("shop all bags");
+  }
+
+  function logPreOrders() {
+      console.log("pre-orders");
+  }
+
   return (
-      <h1>Handbags & Purses</h1>
+      <>
+        <header>
+        <h1>Handbags & Purses</h1>
+        <nav>
+          <button type="button" onClick={logToTheCollection}>to the collection</button>
+          <button type="button" onClick={logShopAllBags}>shop all bags</button>
+          <button type="button" onClick={logPreOrders}>pre-orders</button>
+        </nav>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+      </>
   )
 }
 
